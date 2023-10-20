6fd1b44ce93333db4ccbbaeb805014f00e432459
Andrew Morton <andrew.morton@mongodb.com>
2023-10-20 03:29:09 +0000

    WT-10957 Try removing lfence and sfence on x86

--- src/include/gcc.h
+++ src/include/gcc.h
@@ -164,16 +164,15 @@ WT_ATOMIC_FUNC(size, size_t, size_t *vp, size_t v)
     do {                                         \
         __asm__ volatile("mfence" ::: "memory"); \
     } while (0)
-#define WT_READ_BARRIER()                        \
-    do {                                         \
-        __asm__ volatile("lfence" ::: "memory"); \
-    } while (0)
+
+/* Try just a compiler barrier. x86's TSO should be enough at runtime */
+#define WT_READ_BARRIER() WT_BARRIER()
+
 /* We only need a compiler barrier for x86 as its memory ordering is strong enough. */
 #define WT_READ_BARRIER_WEAK_MEMORDER() WT_BARRIER()
-#define WT_WRITE_BARRIER()                       \
-    do {                                         \
-        __asm__ volatile("sfence" ::: "memory"); \
-    } while (0)
+
+/* Try just a compiler barrier. x86's TSO should be enough at runtime */
+#define WT_WRITE_BARRIER() WT_BARRIER()
 
 #elif defined(i386) || defined(__i386__)
 #define WT_PAUSE() __asm__ volatile("pause\n" ::: "memory")
